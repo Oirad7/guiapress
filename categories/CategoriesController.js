cd576adb7081e6910c590996ab29aa771df5e0e8
Oirad7 <60646094+Oirad7@users.noreply.github.com>
2023-01-04 15:42:58 -0300

    implementação rota/botão deletar em categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -30,4 +30,23 @@ router.get("/admin/categories", (req, res) => {
     
 });
 
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined) {
+            if(!isNaN(id)){
+                Category.destroy({
+                    where: {
+                        id:id
+                    }
+                }).then(() => {
+                    res.redirect("/admin/categories");
+                });
+            }else{ //se NaN
+                res.redirect("/admin/categories");
+            }
+    }else{ //se NULL
+        res.redirect("/admin/categories");
+    }
+})
+
 module.exports = router;
