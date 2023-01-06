df6345f388823ff361bb687b96425fc42ba01092
Oirad7 <60646094+Oirad7@users.noreply.github.com>
2023-01-06 16:34:40 -0300

    implementação view de artigos, config. tinymce, botão salvar artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get('/articles', (req, res) => {
     res.send('Rota de artigos')
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('Rota para criar uma nova categoria')
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories})
+    })
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false //não aparecer "executing default select 1+1 as result" no terminal
+    logging: false, //não aparecer "executing default select 1+1 as result" no terminal
+    timezone: "-03:00"
 });
 
 module.exports = connection;
