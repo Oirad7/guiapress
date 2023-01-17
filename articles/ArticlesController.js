6419ddf5bd5b963404206e43558b2dde1ac90238
Oirad7 <60646094+Oirad7@users.noreply.github.com>
2023-01-17 09:50:35 -0300

    criação router/view edição de arquivos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -52,4 +52,46 @@ router.post("/articles/delete", (req, res) =>{
     }
 });
 
+
+
+router.get("/admin/articles/edit/:id",  (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {categories: categories, article: article})
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    },{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        console.log("erro: "+err);
+        res.redirect("/");
+    });
+});
+
+
 module.exports = router;
